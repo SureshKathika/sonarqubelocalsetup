9cfd671cb79ad3f2b3b9410dd3e58fab8b09ec9b
Suresh Kumar <sureshkumarkathika@gmail.com>
2024-11-25 23:35:33 +0530

    Initial Commit

---
+++ sonarQubeTestCaseSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sonarQubeTestCaseSetup
+//
+//  Created by Kathika, Suresh Kumar (Cognizant) on 25/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sonarQubeTestCaseSetupTests/sonarQubeTestCaseSetupTests.swift
@@ -0,0 +1,17 @@
+//
+//  sonarQubeTestCaseSetupTests.swift
+//  sonarQubeTestCaseSetupTests
+//
+//  Created by Kathika, Suresh Kumar (Cognizant) on 25/11/24.
+//
+
+import Testing
+@testable import sonarQubeTestCaseSetup
+
+struct sonarQubeTestCaseSetupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
